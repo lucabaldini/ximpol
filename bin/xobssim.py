e44695c339ebc0f3a7873cac9dda4c8ac6c9c8ec
omodei <nicola.omodei@gmail.com>
2015-12-17 14:38:12 -0800

    Improved simple simulator

--- bin/xobssim.py
+++ bin/xobssim.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python
+import time
+
 from ximpol.srcmodel.xSource import xSource
 from ximpol.srcmodel.xGenerator import xGenerator
 from ximpol.srcmodel.xSpectralComponent import xSpectralComponent
 from ximpol.irf.xAeff import xAeff
 from ximpol.irf.xPsf import xPsf
+from ximpol.event.xEvent import xEvent
+from ximpol.event.xEventList import xEventList
 
 import scipy as sp  
 from scipy import interpolate
 from matplotlib import pyplot as plt
 
 if __name__=='__main__':
+    t0=time.time()
     tmin=0
     tmax=10
     emin=1
@@ -29,9 +34,9 @@ if __name__=='__main__':
     print ra0, dec0
     spectrum=xSpectralComponent('spectrum')
     
-    times=sp.linspace(tmin,tmax,100)
-    flux=[]
-    events=[]
+    times  = sp.linspace(tmin,tmax,100)
+    flux   = []
+    events = []
     for t in times:
         spectrum.powerlaw(C(t),gamma(t))
         x,y = aeff.convolve(spectrum)
@@ -41,30 +46,30 @@ if __name__=='__main__':
     
     lc   = interpolate.UnivariateSpline(times,flux,k=1,s=0)
     
-    S=xGenerator(lc,lc.integral)
+    S = xGenerator(lc,lc.integral)
     S.setMinMax(tmin,tmax)
-    events_times=S.generate()
-    events_energies=[]
-    events_position_ra=[]
-    events_position_dec=[]
-    events_photoelectron_angle=[]
+    
+    events_times = S.generate()
+    event_list   = xEventList()
     
     print 'generating %d events...' % len(events_times)
-    for i,evt in enumerate(events_times):
-        #print '%.2f' % (i/(1.0*len(events_times)))
-        spectrum.powerlaw(C(evt),gamma(evt))
-        x,y = aeff.convolve(spectrum)
-        f   = interpolate.UnivariateSpline(x,y,k=1,s=0)
+    for i,event_time in enumerate(events_times):
+        _event      = xEvent()
+        _event.time = event_time
+        
+        spectrum.powerlaw(C(event_time),gamma(event_time))
+        energy_array,count_spectrum_array =  aeff.convolve(spectrum)
+        
+        f   = interpolate.UnivariateSpline(energy_array,count_spectrum_array,k=1,s=0)
         S   = xGenerator(f,f.integral)
         S.setMinMax(emin,emax)
-        events_energies.append(S.generate(1)[0])
-        #ra,dec=mySource.getRADec()
-        
-        events_position_ra.append(ra0+psf())
-        events_position_dec.append(dec0+psf())
-        events_photoelectron_angle.append(0.0)        
+        _event.energy=S.generate(1)[0]
+        _ra,_dec=psf.smear(ra0,dec0)
+        _event.setRADec(_ra,_dec)
+        _event.angle=0.0
+        event_list.fill(_event)        
         pass
-    
+    event_list.write_fits('test.fits')
     fig=plt.figure(figsize=(10,10),facecolor='w')
     #plt.subplots_adjust(hspace=0.001)
     ax = plt.subplot(221)
@@ -72,22 +77,22 @@ if __name__=='__main__':
     plt.xlabel('Time [s]')
 
     ax = plt.subplot(222)
-    plt.hist2d(events_position_ra,events_position_dec,100,range=[[ra0-0.05,ra0+0.05],[dec0-0.05,dec0+0.05]])
+    plt.hist2d(event_list.ra_array,event_list.dec_array,100,range=[[ra0-0.05,ra0+0.05],[dec0-0.05,dec0+0.05]])
     plt.xlabel('RA.')
     plt.ylabel('Dec.')
 
     ax = plt.subplot(223)
-    plt.plot(events_times,events_energies,'o')
+    plt.plot(event_list.time_array,event_list.energy_array,'o')
     plt.yscale('log')
     plt.xlabel('Time [s]')
     plt.ylabel('Energy [keV]')
 
     ax = plt.subplot(224)
-    plt.hist(events_energies,bins=sp.logspace(0,1,50),histtype='step')
+    plt.hist(event_list.energy_array,bins=sp.logspace(0,1,50),histtype='step')
     plt.xscale('log')
     plt.yscale('log')
     plt.xlabel('Energy [keV]')
-
+    print time.time()-t0
     plt.show()
     
     #spectrum.plot(sp.linspace(1,10,100))
--- irf/xPsf.py
+++ irf/xPsf.py
@@ -3,9 +3,16 @@ import scipy as sp
 from astropy import units as u
 
 class xPsf():
-    def __init__(self):
-        self.psf=sp.random.normal
+    def __init__(self,rms_arcsec=15.):
+        self.psf = sp.random.normal
+        self.rms = rms_arcsec/3600.
+        
         pass
     def __call__(self,energy=0,theta=0):
         
-        return self.psf(0.0,15.0)/3600.
+        return self.psf(0.0,self.rms)
+    
+    def smear(self,ra0,dec0):
+        radius=self()
+        angle=sp.random.uniform(0,2*sp.pi)
+        return  ra0+radius*sp.cos(angle),dec0+radius*sp.sin(angle),
