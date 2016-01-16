4cdd20910d640e11b6cf6de58eeaf85054802f1d
Luca Baldini <luca.baldini@pi.infn.it>
2016-01-15 16:08:29 -0800

    Initial import.

---
+++ ximpol/test/test_energy_dispersion.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+#
+# Copyright (C) 2015, the ximpol team.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU GengReral Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+
+"""Unit test for the energy dispersion facility.
+"""
+
+import os
+import numpy
+import unittest
+
+from ximpol import XIMPOL_IRF
+from ximpol.detector.xipe import GPD_ERES_FILE_PATH, IRF_NAME
+from ximpol.irf.rmf import xEnergyDispersion
+from ximpol.core.spline import xInterpolatedUnivariateSplineLinear
+from ximpol.utils.matplotlib_ import pyplot as plt
+from ximpol.utils.matplotlib_ import overlay_tag, save_current_figure
+
+
+class TestEnergyDispersion(unittest.TestCase):
+
+    """Unit test for xModulationFactor.
+    """
+
+    @classmethod
+    def setUpClass(self):
+        """Setup---here we essentially create the modulation factor.
+        """
+        self.measx, self.measy = numpy.loadtxt(GPD_ERES_FILE_PATH, unpack=True)
+        file_path = os.path.join(XIMPOL_IRF,'fits','xipe_baseline.rmf')
+        self.edisp = xEnergyDispersion(file_path)
+
+    def test_rvs(self, num_events=100000):
+        """
+        """
+        mc_energy = 10
+        _ppf = self.edisp.matrix.vppf.vslice(mc_energy)
+        _e = numpy.zeros(num_events)
+        _e.fill(mc_energy)
+        _slice = self.edisp.matrix.slice(mc_energy)
+        _ch = self.edisp.matrix.rvs(_e)
+        n, bins, patches = plt.hist(_ch, bins=numpy.linspace(0, 255, 256),
+                                    histtype='step')
+        bin_width = (bins[1] - bins[0])
+        scale = n.sum()*bin_width/_slice.norm()
+        plt.plot(_slice.x, scale*_slice.y)
+        plt.show()
+
+
+if __name__ == '__main__':
+    unittest.main()
