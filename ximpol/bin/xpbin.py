1b5125a3743de5a14816b9b032f9ccbbe03e74f9
Luca Baldini <luca.baldini@pi.infn.it>
2016-03-01 15:16:21 -0800

    Minor.

--- ximpol/bin/xpbin.py
+++ ximpol/bin/xpbin.py
@@ -37,7 +37,7 @@ BIN_ALG_DICT = {
 }
 BIN_ALGS = BIN_ALG_DICT.keys()
 BIN_ALGS.sort()
-TBIN_ALGS = ['FILE', 'LIN', 'LOG', 'SNR']
+TBIN_ALGS = ['FILE', 'LIN', 'LOG']
 EBIN_ALGS = ['FILE', 'LIN', 'LOG', 'EQP']
 PRJCTS = ['AIT', 'ZEA', 'ARC', 'CAR', 'GLS', 'MER', 'NCP', 'SIN', 'STG', 'TAN']
 COORD_SYS = ['CEL', 'GAL']
