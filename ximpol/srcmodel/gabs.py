ba0dc6bb264f568e154f21aac22266a79b654545
Luca Baldini <luca.baldini@pi.infn.it>
2016-06-13 16:35:09 +0200

    Cleanup of the gabs code.

--- ximpol/srcmodel/gabs.py
+++ ximpol/srcmodel/gabs.py
@@ -18,118 +18,51 @@
 
 import os
 import numpy
-import astropy.io.fits as pf
-from astropy.coordinates import SkyCoord, Galactic
+
+from astropy.io import fits
+from astropy.coordinates import SkyCoord
 from astropy.wcs import WCS
 
 from ximpol import XIMPOL_SRCMODEL
-from ximpol.utils.logging_ import logger,abort
+from ximpol.utils.logging_ import logger, abort
 from ximpol.core.spline import xInterpolatedUnivariateSplineLinear
 
 
-def get_column_density(ra, dec, HImap='LAB', lonlat = False):
-    """Extract the column density from a HI map.
 
-        Arguments
-        ---------
-        ra : float
-            Right Ascension of the source [deg].
+def mapped_column_density(ra, dec, map_name='LAB'):
+    """Return the mapped column density at a given position in the sky.
 
-        dec: float
-            Declination of the source [deg].
+    The value is read from one of the available input maps. Note that the
+    data in the maps are stored in Galactic coordinates, while we're
+    giving Celestial coordinates in input, here---the transformation is
+    handled internally.
 
-        map: str
-            The label of the HI column density map to use. 
-             Default: 'LAB' -> uses h1_nh_LAB.fits (LAB survay)
-             Other options: 'DL' -> uses h1_nh_DL.fits (Dickey & Lockman)
+    Arguments
+    ---------
+    ra : float
+        Right ascension of the source (in decimal degrees).
 
-        lonlat: Bool
-             If True, ra and dec parameters are interpretated as galactic 
-             L and B [deg]
+    dec: float
+        Declination of the source (in decimal degrees).
 
-        Warning
-        -------
-        Both the two HI column density maps available are expressed in galactic 
-        coordinates. Hence, if RA and DEC are given, a convertion must be 
-        performed.
+    map: str
+        The HI column density map to use. Can be either 'LAB' (LAB survey)
+        or 'DL' (Dickey & Lockman).
     """
-    nh_file_path = os.path.join(XIMPOL_SRCMODEL,'fits','h1_nh_%s.fits'%HImap)
-    if not os.path.exists(nh_file_path):
-        abort('ATT: HI column density maps %s not found!'%nh_file_path)
-    if lonlat:
-        logger.info('Getting HI column density for L = %f deg, B = %f deg, from %s HI map'\
-                    %(ra,dec,HImap))
-        column_density = nh_from_map(ra,dec,nh_file_path)
-    else:
-        logger.info('Getting HI column density for RA = %f deg, DEC = %f deg, from %s HI map'\
-                    %(ra,dec,HImap))
-        l,b = radec2lonlat(ra,dec)
-        column_density = nh_from_map(l,b,nh_file_path)
-    return column_density
-
-
-def get_galactic_absorption(energy, column_density):
-    """Returns the galactic transmission factor at a given energy and 
-       column density.
-
-        Arguments
-        ---------
-        energy : float
-            Energy value [keV].
-
-        column_density: float
-             Column density at the source position
-    """
-    xsec = XsectionISM(energy)
-    trans_factor = TransmissionFactor(xsec,column_density)
-    return trans_factor
-    
-
-def radec2lonlat(ra, dec):
-    """Function to get L and B if RA and DEC are given.
-    """
-    c = SkyCoord(ra,dec,unit='deg')
-    cc = c.galactic
-    cc.transform_to(Galactic)
-    l,b = cc.l.degree, cc.b.degree
-    return l,b
-
-
-def nh_from_map(l, b, file_path):
-    """Function to get NH value given RA and DEC and the HI map.
-    """
-    hdu = pf.open(file_path)
-    w = WCS(hdu[0].header)
-    row,col = w.wcs_world2pix(l,b,1)
-    data = hdu[0].data
-    column_density = data[int(col)][int(row)]
-    return column_density
-
-
-def XsectionISM(energy):
-    """Returns the absorbtion x-section in cm^2 vs the energy in keV 
-    Cross-section are defined between 0.03 keV and 10 keV
-    """
-    xsec_file = os.path.join(XIMPOL_SRCMODEL,'ascii','XsecFitParam.txt')
-    if not os.path.exists(xsec_file):
-        abort('ATT: cross-section fit parameters file %s not found!'%xsec_file)
-    E_lo,E_hi,c0,c1,c2 = numpy.loadtxt(xsec_file,delimiter=',',unpack=True)
-    bin_num = 0.
-    for i in range(0,len(E_lo)):
-        if E_lo[i] <= energy <= E_hi[i]:
-            bin_num = i
-            pass
-        else:
-            continue
-    cross_section = (c0[bin_num]+c1[bin_num]*energy+c2[bin_num]*energy**2)\
-               *energy**(-3)*10**(-24)
-    return cross_section
-
-def TransmissionFactor(cross_section, column_density):
-    """Calculate the transmission factor for a given x-section and nh
-    """
-    factor = numpy.exp(-cross_section*column_density)
-    return factor
+    # Make sure the map name makes sense.
+    assert map_name in ['LAB', 'DL']
+    # Transform from Celestial to Galactic coordinates.
+    gal_coords = SkyCoord(ra, dec, unit='deg').galactic
+    l, b = gal_coords.l.degree, gal_coords.b.degree
+    # Open the selected input FITS map and grab the values.
+    file_path = os.path.join(XIMPOL_SRCMODEL,'fits','h1_nh_%s.fits' % map_name)
+    if not os.path.exists(file_path):
+        abort('Could not find %s' % file_path)
+    hdu_list = fits.open(file_path)
+    _wcs = WCS(hdu_list[0].header)
+    _data = hdu_list[0].data
+    row, col = [int(item) for item in _wcs.wcs_world2pix(l, b, 1)]
+    return _data[col, row]
 
 
 
@@ -176,8 +109,8 @@ class xpeInterstellarAbsorptionModel:
         # Calculate the cross section values---compact, isn't it?
         _y = 1.0e-24*(c0[_bin] + c1[_bin]*_x + c2[_bin]*(_x**2.))/(_x**3.)
         # And, finally, build the basic spline underlying the model.
-        _fmt = dict(xname='Energy', xunits='keV',
-                    yname='ISM absorption cross section', yunits='cm$^2$')
+        _fmt = dict(xname='Energy', xunits='keV', yname='$\\sigma_{abs}$',
+                    yunits='cm$^2$')
         self.xsection = xInterpolatedUnivariateSplineLinear(_x, _y, **_fmt)
 
     def __call__(self, energy):
@@ -191,6 +124,19 @@ class xpeInterstellarAbsorptionModel:
             calculated.
         """
         return self.xsection(energy)
+
+    def xsection_ecube(self):
+        """Return a spline with the values of absorption cross section
+        multiplied by the cube of the energy, as a function of the energy.
+        
+        This is essentially the same plot in Figure 1 of the original paper.
+        """
+        _x = self.xsection.x
+        _y = self(_x)*(_x**3.)
+        _fmt = dict(xname='Energy', xunits='keV',
+                    yname='$\\sigma_{abs} \\times E^3$',
+                    yunits='cm$^2$~keV$^3$')
+        return xInterpolatedUnivariateSplineLinear(_x, _y, **_fmt)
 
     def transmission_factor(self, column_density):
         """Return the transmission factor for a given column density.
---
+++ ximpol/test/test_gabs.py
@@ -0,0 +1,64 @@
+#!/urs/bin/env python
+#
+# Copyright (C) 2015--2016, the ximpol team.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU GengReral Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+
+import sys
+import os
+
+from ximpol.srcmodel.gabs import mapped_column_density,\
+    xpeInterstellarAbsorptionModel
+from ximpol.utils.logging_ import logger, abort
+from ximpol.utils.matplotlib_ import pyplot as plt
+from ximpol.utils.matplotlib_ import overlay_tag, save_current_figure
+
+
+
+def main(interactive=False):
+    """Make some ISM absorption related plots.
+    """
+    model = xpeInterstellarAbsorptionModel()
+
+    plt.figure()
+    model.xsection.plot(logx=True, logy=True, show=False)
+    save_current_figure('gabs_xsection.png', clear=False)
+
+    plt.figure()
+    model.xsection_ecube().plot(logx=True, show=False)
+    save_current_figure('gabs_xsection_ecube.png', clear=False)
+
+    plt.figure()
+    ra, dec = 10.684, 41.269
+    column_density = mapped_column_density(ra, dec, 'LAB')
+    trans = model.transmission_factor(column_density)
+    trans.plot(logx=True, show=False, label='$n_H = $%.3e' % column_density)
+    plt.legend(loc='upper left')
+    save_current_figure('gabs_trans_lab.png', clear=False)
+
+    plt.figure()
+    for column_density in [1.e20, 1.e21, 1.e22, 1.e23]:
+        trans = model.transmission_factor(column_density)
+        trans.plot(logx=True, show=False, label='$n_H = $%.1e' % column_density)
+    plt.legend(loc='upper left')
+    save_current_figure('gabs_trans_samples.png', clear=False)
+    
+    if interactive:
+        plt.show()
+
+        
+if __name__ == '__main__':
+    main(interactive=sys.flags.interactive)
